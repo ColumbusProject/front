e4261ce6db278a3ba18cb6cf3b1e0b1f279b6e14
HellowBong <jhjb93@naver.com>
2023-10-25 18:55:40 +0900

    google api update

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import './App.css';
 import Authentication from './views/Authentication';
 import Main from './views/Main';
 import Detail from './views/Board/Review/Detail';
+import ItineraryBoardWrite from './views/Board/Itinerary/Write';
 
 
 function App() {
   return (
     <div>
-      < Detail />
-    </div>
+    < ItineraryBoardWrite />
+  </div>
   );
 }
 
