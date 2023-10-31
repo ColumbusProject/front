d2a3c5cc4ccb2d7480a19df8d4474554fde951eb
Sojinlee777 <imadeit777@naver.com>
2023-10-31 13:37:26 +0900

    sojin job

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import './App.css';
 import Authentication from './views/Authentication';
-import Main from './views/Main';
 import Detail from './views/Board/Review/Detail';
 import ItineraryBoardWrite from './views/Board/Itinerary/Write';
 import Header from './layouts/Header';
-
 
 function App() {
   return (
