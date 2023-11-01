eb345f062bb28ab13620efbec9aa7d838b87de12
Sojinlee777 <imadeit777@naver.com>
2023-11-01 17:41:21 +0900

    2023-11-01

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import './App.css';
-import Authentication from './views/Authentication';
-import Detail from './views/Board/Review/Detail';
-import Write from './views/Board/Write';
-import ItineraryBoardWrite from './views/Board/Itinerary/Write';
-import Header from './layouts/Header';
 import Main from './views/Main';
+// import Authentication from './views/Authentication';
+// import Detail from './views/Board/Review/Detail';
+// import Write from './views/Board/Write';
+// import ItineraryBoardWrite from './views/Board/Itinerary/Write';
+// import Header from './layouts/Header';
 
 function App() {
   return (
     <div className='test'>
-      < Authentication />
+      < Main/>
     </div>
   );
 }
