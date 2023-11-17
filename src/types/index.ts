0f38aca093ff55a762a4af7f495c0af4ddd76ce1
HellowBong <jhjb93@naver.com>
2023-11-17 13:38:04 +0900

    231117-bong-update

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,6 +5,7 @@ import BoardListItem from "./board-list-item.interface";
 import LoginUser2 from "./login-user2.interface";
 import CommentListItem from "./comment-list-item.interface";
 import ItineraryBoardItem from "./itinerary-board-list-item.interface";
+import CommentListItem02 from "./comment-list-item-02.interface";
 
 export type {
   User,
@@ -13,5 +14,6 @@ export type {
   BoardListItem,
   LoginUser2,
   CommentListItem,
+  CommentListItem02,
   ItineraryBoardItem,
 }
