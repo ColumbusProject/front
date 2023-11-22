8e9d18538189e489c0a2fc53090e6df6b9a7e396
이소똥의 Mac Book <esottong-macbook@isottong-uiui-MacBookAir.local>
2023-11-22 14:57:50 +0900

    a lot of happy errors

---
+++ src/components/Trade/Body/Body.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Title from '../1. Title/Title'
+
+
+
+
+
+function Body() {
+  console.log() 
+  return (
+    <div>
+      <Title />
+    
+        
+      
+    </div>
+
+
+  )
+}
+
+export default Body
---
+++ src/components/Trade/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './component/1.Header/Header';
+import Body from './component/3.Body/TSXs/Body';
+import Footer from './component/2.Footer/Footer';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+    
+  )
+};
+
+export default App
+
