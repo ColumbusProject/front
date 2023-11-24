2150299ab5cb10adc1bab43dda3a0989ba8739db
HellowBong <jhjb93@naver.com>
2023-11-24 20:37:23 +0900

    231124-bong-footer-update

--- src/layouts/Container/index.tsx
+++ src/layouts/Container/index.tsx
@@ -14,6 +14,7 @@ export default function Container() {
     <>
       <Header />
       <Outlet />
+      <Footer />
     </>
   )
 }
--- src/layouts/Footer/index.tsx
+++ src/layouts/Footer/index.tsx
@@ -5,7 +5,12 @@ export default function Footer() {
 
   //          render: 푸터 컴포넌트 렌더링          //
   return(
-    <></>
+    <div className='footer-wrapper'>
+      <div className='footer-container'>
+        <div className='footer-title-text-box'>{'Columbus'}</div>
+        <div className='footer-content-text-box'>{'Copyright ⓒ 2023-2023 KOREA IT Company All rights reserved.'}</div>
+      </div>
+    </div>
   )
 
-}
+} 
