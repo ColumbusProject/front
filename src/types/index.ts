a2b42179dcdf97430cdd4096e5ee7d9b98ad810e
kimyujung <yjangel3702@gmail.com>
2023-11-26 05:42:28 +0900

    1126 05:42

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,6 +5,8 @@ import BoardListItem from "./interface/board-list-item-interface";
 import CommentListItem from "./interface/comment-list-item.interface";
 import ItineraryBoardItem from "./interface/itinerary-board-list-item.interface";
 import FavoriteListItem from "./interface/favorite-list-item.interface";
+import TradeLatestList from "./interface/trade-latest-list";
+import TradeListItem from "./interface/trade-list-item";
 
 export type {
   User,
@@ -13,5 +15,7 @@ export type {
   BoardListItem,
   CommentListItem,
   ItineraryBoardItem,
-  FavoriteListItem
+  FavoriteListItem,
+  TradeLatestList,
+  TradeListItem
 }
